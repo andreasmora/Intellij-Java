87ca31f86faa769e005aebce569ba631588c57c9
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-13 15:44:23 -0300

    Projeto Aulas Java

--- CursoJavaBasico/JavaExercicios3/src/com/java/Ex118.java
+++ CursoJavaBasico/JavaExercicios3/src/com/java/Ex118.java
@@ -4,6 +4,7 @@
 
     package com.java;
 
+    import java.util.Random;
     import java.util.Scanner;
 
     public class Ex118 {
@@ -12,6 +13,33 @@
 
             int[][] numerosAleatorios = new int[4][4];
 
+            Random numeroRandom = new Random(); // funcao valores aleatorios
+
+            for (int i=0; i<numerosAleatorios.length; i++){
+                for (int j=0; j<numerosAleatorios[i].length; j++) {
+                numerosAleatorios[i][j] = numeroRandom.nextInt(100);
+
+                }
+            }
+
+            int maior = 0;
+            int linha = 0;
+            int coluna = 0;
+
+            for (int i=0; i<numerosAleatorios.length; i++){
+                for (int j=0; j<numerosAleatorios[i].length; j++) {
+                    if (numerosAleatorios[i][j] > maior){
+                        maior = numerosAleatorios[i][j];
+                        linha = i;
+                        coluna = j;
+                    }
+
+                }
+            }
+
+            System.out.println("Maior valor: " + maior);
+            System.out.println("Linha: " + linha);
+            System.out.println("Coluna: " + coluna);
 
 
 
