91d8fd56e3fe7ecb058ad79282ab1d4d71d20cec
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-13 16:00:51 -0300

    Projeto Aulas Java

--- CursoJavaBasico/JavaExercicios3/src/com/java/Ex118.java
+++ CursoJavaBasico/JavaExercicios3/src/com/java/Ex118.java
@@ -37,14 +37,15 @@
                 }
             }
 
+            for (int i=0; i<numerosAleatorios.length; i++) {
+                for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                    System.out.print(numerosAleatorios[i][j] + " ");
+                }
+                System.out.println();
+            }
             System.out.println("Maior valor: " + maior);
             System.out.println("Linha: " + linha);
             System.out.println("Coluna: " + coluna);
-
-
-
-
-
 
         }
     }
