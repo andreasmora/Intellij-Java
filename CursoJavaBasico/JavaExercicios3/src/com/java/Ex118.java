85e16a1ccbf2702acacd69545b9e6c6b8509f2fe
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-13 13:39:48 -0300

    Projeto Aulas Java

---
+++ CursoJavaBasico/JavaExercicios3/src/com/java/Ex118.java
@@ -0,0 +1,14 @@
+//Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0-
+//9. Após isso determine o maior número da matriz e a sua posição
+//(linha, coluna).
+
+package com.java;
+
+import java.util.Scanner;
+
+public class Ex118 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
