92ad0a91b8ff507cae4e1db8b99d9393528d34a0
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-13 17:03:40 -0300

    Projeto Aulas Java

--- CursoJavaBasico/JavaExercicios3/src/com/java/Ex120.java
+++ CursoJavaBasico/JavaExercicios3/src/com/java/Ex120.java
@@ -2,14 +2,21 @@
 //3x3. Após a captura imprima a matriz criada e encontre a
 //quantidade de números pares, a quantidade de números ímpares
 
-package com.java;
+    package com.java;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Ex120 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public class Ex120 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
 
+            int[][] numeros = new int[3][3];
+            for (int i=0; i<numeros.length; i++){
+                for (int j=0; j<numeros[i].length; j++){
 
+                    System.out.println("Entre com o valor da posicao [" + i + " , " + j + " ");
+                }
+            }
+
+        }
     }
-}
