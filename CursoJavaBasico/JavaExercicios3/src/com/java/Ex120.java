1125c765c48d07e8c84fb1627ff47faa49040146
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-14 09:31:36 -0300

    Projeto Aulas Java

--- CursoJavaBasico/JavaExercicios3/src/com/java/Ex120.java
+++ CursoJavaBasico/JavaExercicios3/src/com/java/Ex120.java
@@ -14,9 +14,30 @@
             for (int i=0; i<numeros.length; i++){
                 for (int j=0; j<numeros[i].length; j++){
 
-                    System.out.println("Entre com o valor da posicao [" + i + " , " + j + " ");
+                    System.out.println("Entre com o valor da posicao [" + i + " , " + j + "]");
+                    numeros[i][j] = scan.nextInt();
                 }
             }
+
+            int qtdPares = 0;
+            int qtdImpares = 0;
+            for (int i=0; i<numeros.length; i++) {
+                for (int j = 0; j < numeros[i].length; j++) {
+                    if (numeros[i][j] % 2 == 0){
+                        qtdPares++;
+                    } else {
+                        qtdImpares++;
+                    }
+                }
+            }
+            for (int i=0; i<numeros.length; i++){
+                for (int j=0; j<numeros[i].length; j++){
+                    System.out.print(numeros[i][j] + "  ");
+                }
+                System.out.println();
+            }
+            System.out.println("Quantidade de pares: " + qtdPares);
+            System.out.println("Quantidade de impares: " + qtdImpares);
 
         }
     }
