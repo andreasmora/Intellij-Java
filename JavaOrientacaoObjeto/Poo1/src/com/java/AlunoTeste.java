ffcca96f088281f5b9d7f6d7ee5759448c9ecd6a
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-18 17:45:19 -0300

    Aulas Java Orientacao Objetos

---
+++ JavaOrientacaoObjeto/Poo1/src/com/java/AlunoTeste.java
@@ -0,0 +1,15 @@
+// Escreva uma classe para representar um aluno. Adicione atributos relacionados as caracteristicas
+// de um aluno, como nome, matricula, curso que esta matriculado, nome de 03 disciplinas que esta cursando
+//nome de 03 disciplinas que esta cursando e as notas dessas 03 disciplinas Desenvolva um metodo para verificar
+// se o aluno estA aprovado ( nota maior ou igual a 7) em uma determinada disciplina. escreva um programa
+//para testar essa classe que pede as informacoes do aluno ao usuario e ao final informa o nome das disciplnas , mostra
+// as notas e mostra se o aluno foi aprovado ou nao.
+
+
+package com.java;
+
+public class AlunoTeste {
+    public static void main(String[] args) {
+
+    }
+}
