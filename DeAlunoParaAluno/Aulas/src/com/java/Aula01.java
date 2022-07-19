3f9cf3860a2082be9c9fa81642ca80ddb3fb25c3
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-19 12:06:23 -0300

    Aulas em Java

--- DeAlunoParaAluno/Aulas/src/com/java/Aula01.java
+++ DeAlunoParaAluno/Aulas/src/com/java/Aula01.java
@@ -1,5 +1,18 @@
 package com.java;
 
 public class Aula01 {
+    public static void main(String[] args) {
+
+        int x = 50;
+        int y = 20;
+
+        int soma = x + y;
+
+        System.out.println("Valor de X = " + x);
+        System.out.println("Valor de Y = " + y);
+        System.out.println("Total de x + y = " + soma);
+
+
+    }
 
 }
