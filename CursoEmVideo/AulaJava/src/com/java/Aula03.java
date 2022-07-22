cc2169905da32e6baae0a20019ecf2f4fd8c4ae5
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-22 11:31:52 -0300

    Aulas em Java

---
+++ CursoEmVideo/AulaJava/src/com/java/Aula03.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Aula03 {
+    public static void main(String[] args) {
+
+        // SAIDA DE DADOS..
+        int idade = 30;
+
+        System.out.printf("Andre tem %d anos de idade\n\n",idade);
+        System.out.println("Andre tem " + idade + " anos de idade!");
+        System.out.println("\tTabulacao");
+
+    }
+}
---
+++ CursoEmVideo/AulaJava/src/com/java/Aula04.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Aula04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //VARIAVEIS...
+
+        /*
+        int numero;
+
+        numero = 10;
+        System.out.println(numero);
+
+        numero = 100;
+        System.out.println(numero);
+
+        //CONSTANTES...
+        final int horasDia = 24;
+        int dias = 10;
+
+        int total = dias * horasDia;
+        System.out.printf("%d dias tem o total de %d horas", dias, total); */
+
+
+    }
+}
