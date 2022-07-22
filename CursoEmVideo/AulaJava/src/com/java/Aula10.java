5dfeea65e96024962ca026a27ef788fdb630ae3a
andreasmora <andrealvesmoraes75@gmail.com>
2022-07-22 17:25:05 -0300

    Aulas em Java

---
+++ CursoEmVideo/AulaJava/src/com/java/Aula10.java
@@ -0,0 +1,36 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Aula10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        boolean num1, num2;
+        num1 = true;
+        num2 = false;
+
+
+        // operador e: &&
+        /*
+        v e v = v
+        v e f = f
+        f e v = f
+        f e f = f
+         */
+
+        // operador ou: ||
+        /*
+        v ou v = v
+        v ou f = v
+        f ou v = v
+        f ou f = f
+         */
+
+
+
+
+
+
+    }
+}
